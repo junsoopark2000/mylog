3b1f61f14933c3c32d4c29a29a0114389bede101
Junsoo Park <junsoopark2000@gmail.com>
2023-05-29 00:06:39 +0900

    give options for each file handler and stream handler

--- mylog.py
+++ mylog.py
@@ -7,9 +7,12 @@ import logging.handlers
 from dotenv import load_dotenv
 
 
-DEFAULT_LOG_FILE = "./default.log"
+DEFAULT_LOG_PATH = "./"
+LOG_FILE_NAME = "app.log"
 LOG_FORMAT = "%(asctime)s [%(levelname)-8s] - %(name)8s.(%(lineno)3d) - %(message)s"
 DATE_FMT = "%Y/%m/%d %H:%M:%S"
+
+load_dotenv()
 
 
 def get_stream_handler():
@@ -32,27 +35,42 @@ def get_file_handler():
 
 def get_log_path_file() -> str:
     """get log path and filename"""
-    path_and_filename: str = os.getenv("LOG_PATH_AND_FILENAME")
-    return DEFAULT_LOG_FILE if path_and_filename is None else path_and_filename
+
+    log_path: str = os.getenv("LOG_PATH")
+
+    if log_path is None or not log_path:  # undefined or empty string
+        log_path = os.getcwd()
+
+    elif not os.path.exists(log_path):
+        os.makedirs(log_path, exist_ok=True)
+
+    return f"{log_path}/{LOG_FILE_NAME}"
 
 
 def get_log_level_in_config() -> int:
     """Returns: logging.DEBUG | logging.INFO | logging.WARNING | logging.ERROR | logging.CRITICAL"""
-    load_dotenv()
+
     log_strategies = {
-        "DEBUG": logging.DEBUG,
-        "INFO": logging.INFO,
-        "WARNING": logging.WARNING,
-        "ERROR": logging.ERROR,
-        "CRITICAL": logging.CRITICAL,
+        "debug": logging.DEBUG,
+        "info": logging.INFO,
+        "warning": logging.WARNING,
+        "error": logging.ERROR,
+        "critical": logging.CRITICAL,
     }
-    return log_strategies.get(os.getenv("LOG_LEVEL").upper(), logging.INFO)
+
+    return log_strategies.get(os.getenv("LOG_LEVEL").lower(), logging.INFO)
 
 
 def get_logger(name: str) -> logging.Logger:
     """returns a logger for each module(.py)"""
+
     logger = logging.getLogger(name)
     logger.setLevel(get_log_level_in_config())
-    logger.addHandler(get_stream_handler())
-    logger.addHandler(get_file_handler())
+
+    if os.getenv("LOG_PRINT_ON_FILE").lower() == "true":
+        logger.addHandler(get_file_handler())
+
+    if os.getenv("LOG_PRINT_ON_CONSOLE").lower() == "true":
+        logger.addHandler(get_stream_handler())
+
     return logger
